d28f3609236e858ac59f3cee9330cffa9c6a3616
E4lix <Ealix95@gmail.com>
2024-12-04 11:41:17 +0100

    Corretion Processus Pere Fils

--- TD7/Exo2/Exo2.c
+++ TD7/Exo2/Exo2.c
@@ -3,25 +3,44 @@
 #include <pthread.h>
 #include <unistd.h>
 
+void *processusPere();
+void *processusFils();
+
+int x = 0;
+
 int main(int argc, char *argv[]){
-    int x = 0;
-    int pid;
 
-    pid = fork();
-    printf("Valeur de fork = %d \n", pid);
-    printf("Processus %d de père %d \n", (int)getpid(), (int)getppid());
+    // int pid;
 
-    if(pid == 0){
-        x = 25;
-        printf("Valeur de x pour le fils : %d \n", x);
-        printf("Fin du processus fils \n");
-    }
-    else {
-        while(x != 25){
-            sleep(2);
-            printf("Valeur de x pour le père : %d \n", x);
-        }
-        printf("Fin du processus père \n");
-    }
+    pthread_t tid_Pere, tid_Fils;
+
+    // pid = fork();
+    // printf("Valeur de fork = %d \n", pid);
+    // printf("Processus %d de père %d \n", (int)getpid(), (int)getppid());
+
+    pthread_create(&tid_Pere, NULL, processusPere, NULL);
+    pthread_create(&tid_Fils, NULL, processusFils, NULL);
+
+    pthread_join(tid_Pere, NULL);
+    // pthread_join(tid_Fils, NULL);
+
     return EXIT_SUCCESS;
+}
+
+void *processusPere(){
+    while(x != 25){
+        sleep(4);
+        printf("Valeur de x pour le père : %d \n", x);
+    }
+    
+    printf("Fin du processus père \n");
+    return NULL;
+}
+
+void *processusFils(){
+    printf("Valeur de x pour le fils : %d \n", x);
+    printf("Fin du processus fils \n");
+    x = 25;
+
+    return NULL;
 }
